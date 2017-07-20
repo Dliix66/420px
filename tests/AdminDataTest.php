d416cc62e7eae8708c72e9ac718d8d43a1f7039d
Romain Jouffret <r.nassty@gmail.com>
2017-07-20 15:12:23 +0200

    ADD - tests

---
+++ tests/AdminDataTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests;
+use PHPUnit\Framework\TestCase;
+
+
+class AdminDataTest extends TestCase{
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+    }
+
+    public function testIsLogged(){
+        $this->assertEquals(1, 1);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+    }
+}
---
+++ tests/UserDataTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests;
+use PHPUnit\Framework\TestCase;
+
+
+class UserDataTest extends TestCase{
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+       
+    }
+
+    public function testIsLogged(){
+        $this->assertEquals(1, 1);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+    }
+}
